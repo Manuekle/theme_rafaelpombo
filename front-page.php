7c6ca41e531378520be6f67e1a100736471c7eec
Manuekle <elustondo129@gmail.com>
2022-11-08 18:11:22 -0500

    first commit

---
+++ front-page.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Institucion Rafael Pombo</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php get_header(); ?>
+    <section class="pt-28">
+        <section class="grid gap-20 container mx-auto">
+            <div class="flex justify-center flex-col items-center gap-4">
+                <h1 class="text-3xl font-bold text-[#19443D]">Proyecto Educativo Institucional</h1>
+                <p class="px-56 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam amet nobis sunt corporis sapiente porro quidem vitae consectetur magni atque laborum, veritatis provident asperiores ipsam officia enim, dolorem animi sed id ut optio itaque tempore! Temporibus tenetur odit a unde amet quidem, assumenda praesentium est ipsa mollitia id dolore?</p>
+            </div>
+            <div class="flex flex-col gap-12 px-56">
+                <article class="flex flex-row gap-8">
+                    <img src="https://images.pexels.com/photos/5669602/pexels-photo-5669602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-lg">
+                    <span class="flex flex-col gap-2">
+                        <h1 class="font-bold text-xl">MISIÓN</h1>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero modi iste nobis? Explicabo aliquam culpa laboriosam necessitatibus modi! Laborum doloribus ullam temporibus, rem perspiciatis ut, eos maxime aperiam unde accusamus alias sint sunt a ab libero! Vitae, illum! Vel, ea.</p>
+                    </span>
+                </article>
+                <article class="flex flex-row gap-8">
+                    <span class="flex flex-col gap-2">
+                        <h1 class="font-bold text-xl">VISIÓN</h1>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero modi iste nobis? Explicabo aliquam culpa laboriosam necessitatibus modi! Laborum doloribus ullam temporibus, rem perspiciatis ut, eos maxime aperiam unde accusamus alias sint sunt a ab libero! Vitae, illum! Vel, ea.</p>
+                    </span>
+                    <img src="https://images.pexels.com/photos/7092/desk-office-hero-workspace.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-lg">
+                </article>
+            </div>
+            <div class="bg-[#19443D] p-8 rounded-md shadow-md shadow-[#19443D] flex flex-col gap-6">
+                <h1 class="text-center text-2xl font-bold text-white">Proyectos de nuestra institucion</h1>
+                <div class="grid grid-cols-4 gap-8 px-32 py-10">
+                    <article class="col-span-1 bg-white w-full rounded-lg shadow-xl flex flex-col gap-3">
+                        <img src="https://images.pexels.com/photos/764681/pexels-photo-764681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-t-lg rounded-b-none">
+                        <span class="flex flex-col gap-4 px-6 pb-4">
+                            <h1 class="text-lg font-bold text-[#19443D] uppercase">title</h1>
+                            <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam provident exercitationem nobis architecto veritatis? Fugiat vel dignissimos sit ipsam esse?</p>
+                            <button class="flex text-green-500 text-xs underline">
+                                Leer mas
+                            </button>
+                        </span>
+                    </article>
+                    <article class="col-span-1 bg-white w-full rounded-lg shadow-xl flex flex-col gap-3">
+                        <img src="https://images.pexels.com/photos/207756/pexels-photo-207756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-t-lg rounded-b-none">
+                        <span class="flex flex-col gap-4 px-6 pb-4">
+                            <h1 class="text-lg font-bold text-[#19443D] uppercase">title</h1>
+                            <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam provident exercitationem nobis architecto veritatis? Fugiat vel dignissimos sit ipsam esse?</p>
+                            <button class="flex text-green-500 text-xs underline">
+                                Leer mas
+                            </button>
+                        </span>
+                    </article>
+                    <article class="col-span-1 bg-white w-full rounded-lg shadow-xl flex flex-col gap-3">
+                        <img src="https://images.pexels.com/photos/356065/pexels-photo-356065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-t-lg rounded-b-none">
+                        <span class="flex flex-col gap-4 px-6 pb-4">
+                            <h1 class="text-lg font-bold text-[#19443D] uppercase">title</h1>
+                            <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam provident exercitationem nobis architecto veritatis? Fugiat vel dignissimos sit ipsam esse?</p>
+                            <button class="flex text-green-500 text-xs underline">
+                                Leer mas
+                            </button>
+                        </span>
+                    </article>
+                    <article class="col-span-1 bg-white w-full rounded-lg shadow-xl flex flex-col gap-3">
+                        <img src="https://images.pexels.com/photos/887584/pexels-photo-887584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-t-lg rounded-b-none">
+                        <span class="flex flex-col gap-4 px-6 pb-4">
+                            <h1 class="text-lg font-bold text-[#19443D] uppercase">title</h1>
+                            <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam provident exercitationem nobis architecto veritatis? Fugiat vel dignissimos sit ipsam esse?</p>
+                            <button class="flex text-green-500 text-xs underline">
+                                Leer mas
+                            </button>
+                        </span>
+                    </article>
+
+                </div>
+            </div>
+            <div class="grid grid-cols-4 gap-16">
+                <div class="col-span-3 flex flex-col gap-8">
+                    <h1 class="text-2xl font-bold text-[#19443D] underline">Articulos Recientes</h1>
+                    <div class="grid grid-cols-3 gap-8">
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/1205651/pexels-photo-1205651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/2305098/pexels-photo-2305098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/4019754/pexels-photo-4019754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/972513/pexels-photo-972513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/159740/library-la-trobe-study-students-159740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                        <article class="col-span-1 flex flex-col gap-2">
+                            <img src="https://images.pexels.com/photos/2116469/pexels-photo-2116469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full h-56 object-cover rounded-md">
+                            <span class="flex justify-between items-center">
+                                <h1 class="text-sm font-bold text-green-500">Popayan, Cauca</h1>
+                                <h1 class="text-xs">26 octubre 2022</h1>
+                            </span>
+                            <h1 class="text-lg text-[#19443D] font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, similique!</h1>
+                        </article>
+                    </div>
+                    <div class="flex justify-center">
+                        <button class="flex bg-green-600/80 px-6 py-2">
+                            <h1 class="text-md font-bold text-white">
+                                Ver más
+                            </h1>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <h1 class="text-2xl font-bold text-[#19443D] underline">Noticias</h1>
+                </div>
+            </div>
+        </section>
+        <section class="grid bg-[#F3F7F9] pt-10 pb-20 mt-20">
+            <div class="container mx-auto flex flex-col gap-12">
+                <h1 class="text-3xl font-bold text-[#19443D] text-center">Nuestros Docentes</h1>
+                <div class="grid grid-cols-3 gap-8">
+                    <article class="grid grid-cols-2 justify-center">
+                        <span class="col-span-1 flex justify-center"> <img src="https://images.pexels.com/photos/5212317/pexels-photo-5212317.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" class="w-40 h-40 object-cover rounded-full"></span>
+                        <span class="col-span-1 flex flex-col justify-evenly">
+                            <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non consequatur ipsa expedita et porro ipsum natus ullam nulla voluptatibus esse.</p>
+                            <span class="flex flex-col">
+                                <h1 class="text-sm font-bold text-[#19443D]">Kimberly Patel</h1>
+                                <h1 class="text-xs text-zinc-400">
+                                    Lic. en Matemáticas
+                                </h1>
+                            </span>
+                        </span>
+                    </article>
+                    <article class="grid grid-cols-2 justify-center">
+                        <span class="col-span-1 flex justify-center"> <img src="https://images.pexels.com/photos/6326377/pexels-photo-6326377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-40 h-40 object-cover rounded-full"></span>
+                        <span class="col-span-1 flex flex-col justify-evenly">
+                            <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non consequatur ipsa expedita et porro ipsum natus ullam nulla voluptatibus esse.</p>
+                            <span class="flex flex-col">
+                                <h1 class="text-sm font-bold text-[#19443D]">Jeffery Paul</h1>
+                                <h1 class="text-xs text-zinc-400">
+                                    Docente de Español
+                                </h1>
+                            </span>
+                        </span>
+                    </article>
+                    <article class="grid grid-cols-2 justify-center">
+                        <span class="col-span-1 flex justify-center"> <img src="https://images.pexels.com/photos/5905483/pexels-photo-5905483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-40 h-40 object-cover rounded-full"></span>
+                        <span class="col-span-1 flex flex-col justify-evenly">
+                            <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non consequatur ipsa expedita et porro ipsum natus ullam nulla voluptatibus esse.</p>
+                            <span class="flex flex-col">
+                                <h1 class="text-sm font-bold text-[#19443D]">Catherine Hamilton</h1>
+                                <h1 class="text-xs text-zinc-400">
+                                    Docente de Sociales
+                                </h1>
+                            </span>
+                        </span>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <section class="h-full relative bg-fixed bg-center bg-cover bg-no-repeat z-10" style="background-image:linear-gradient(90deg, rgba(25,68,61,0.8295693277310925) 0%, rgba(25,68,61,0.9920343137254902) 100%, rgba(25,68,61,1) 100%), url(https://images.pexels.com/photos/2350925/pexels-photo-2350925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)">
+            <div class="container mx-auto grid grid-cols-2 gap-8 flex-4 py-16 items-center">
+                <div class="col-span-1 flex flex-col gap-3">
+                    <h1 class="font-bold text-white text-3xl">Contacta con nosotros</h1>
+                    <h1 class="font-normal text-white text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos soluta ullam placeat dolores voluptate sit aperiam reprehenderit porro quidem similique.</h1>
+                    <h1 class="font-normal text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, officia.</h1>
+                </div>
+                <div class="col-span-1 bg-white px-8 py-6 rounded-lg flex flex-col gap-6">
+                    <h1 class="font-bold text-black text-2xl">Información de Contacto</h1>
+                    <div class="flex flex-col gap-4">
+                        <span class="flex flex-col gap-1">
+                            <h1 class="font-normal text-black text-sm">Nombre madre/padre o tutor *</h1>
+                            <input type="text" class="border-1 border-[#19443D] rounded-lg px-4 py-2">
+                        </span>
+                        <span class="flex flex-col gap-1">
+                            <h1 class="font-normal text-black text-sm">Apellidos madre/padre o tutor *</h1>
+                            <input type="text" class="border-1 border-[#19443D] rounded-lg px-4 py-2">
+                        </span>
+                        <span class="flex flex-col gap-1">
+                            <h1 class="font-normal text-black text-sm">Correo electrónico de contacto *</h1>
+                            <input type="email" class="border-1 border-[#19443D] rounded-lg px-4 py-2">
+                        </span>
+                        <span class="flex flex-col gap-1">
+                            <h1 class="font-normal text-black text-sm">Teléfono móvil de contacto</h1>
+                            <input type="number" class="border-1 border-[#19443D] rounded-lg px-4 py-2">
+                        </span>
+                        <span class="flex flex-col gap-1">
+                            <h1 class="font-normal text-black text-sm">Tu mensaje *</h1>
+                            <textarea name="" id="" cols="10" rows="5" class="border-1 border-[#19443D] rounded-lg px-4 py-2"></textarea>
+                        </span>
+                    </div>
+                    <span class="flex flex-col">
+                        <h1 class="font-bold text-xs text-zinc-500">INFORMACION BASICA PROTECCION DE DATOS</h1>
+                        <p class="text-xs text-zinc-400">Información adicional y derechos: Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, maxime ipsum excepturi quas suscipit mollitia ad aut iste porro ipsa. <span class="text-green-500 cursor-pointer">Política de Privacidad.</span></p>
+                    </span>
+                    <div class="flex justify-center">
+                        <button class="bg-green-500/80 font-bold text-white px-8 py-2">Enviar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <?php get_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Institucion Rafael Pombo</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="bg-white p-4 w-full shadow-lg fixed z-20">
+        <section class="container mx-auto grid grid-cols-4 gap-4 items-center">
+            <div class="col-span-1 font-bold text-lg">
+                INSTITUCION EDUCATIVA RAFAEL POMBO</div>
+            <div class="col-span-3 flex flex-row justify-end gap-8">
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Inicio
+                    </h1>
+                </span>
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Quienes Somos
+                    </h1>
+                    <span class="material-symbols-outlined text-green-500">
+                        keyboard_arrow_down
+                    </span>
+                </span>
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Recursos Educativos
+                    </h1>
+                    <span class="material-symbols-outlined text-green-500">
+                        keyboard_arrow_down
+                    </span>
+                </span>
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Sedes Educativas
+                    </h1>
+                    <span class="material-symbols-outlined text-green-500">
+                        keyboard_arrow_down
+                    </span>
+                </span>
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Noticias
+                    </h1>
+                    <span class="material-symbols-outlined text-green-500">
+                        keyboard_arrow_down
+                    </span>
+                </span>
+                <span class="flex flex-row items-center cursor-pointer">
+                    <h1 class="text-sm font-bold">
+                        Contactenos
+                    </h1>
+                </span>
+            </div>
+        </section>
+    </header>
+</body>
+
+</html>
